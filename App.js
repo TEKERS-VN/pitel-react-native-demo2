b810c9ef16157cee6470dbc8c4784abf189f7509
Huy Do <thehuyvb@gmail.com>
2022-10-06 13:29:10 +0700

    Init commit

---
+++ App.js
@@ -0,0 +1,180 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import PitelSDK from 'pitel-sdk-webrtc';
+import {
+  mediaDevices,
+  MediaStream,
+  MediaStreamTrack,
+  MediaStreamTrackEvent,
+  RTCIceCandidate,
+  RTCPeerConnection,
+  RTCSessionDescription,
+} from 'pitel-react-native-webrtc';
+
+window.RTCPeerConnection = window.RTCPeerConnection || RTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || RTCIceCandidate;
+window.RTCSessionDescription =
+  window.RTCSessionDescription || RTCSessionDescription;
+window.MediaStream = window.MediaStream || MediaStream;
+window.MediaStreamTrack = window.MediaStreamTrack || MediaStreamTrack;
+window.MediaStreamTrackEvent =
+  window.MediaStreamTrackEvent || MediaStreamTrackEvent;
+window.navigator.mediaDevices = window.navigator.mediaDevices || mediaDevices;
+window.navigator.getUserMedia =
+  window.navigator.getUserMedia || mediaDevices.getUserMedia;
+
+function initSdk() {
+  const sdkOptions = {
+    sipOnly: true,
+    sipDomain: 'mobile.tel4vn.com:50061',
+    wsServer: 'wss://wss-mobile.tel4vn.com:7444',
+    sipPassword: 'Tel4vn.com123@',
+    debug: true,
+  };
+  const sdkDelegates = {
+    onRegistered() {
+      console.log('SIP Register success!');
+      // bindEvents();
+      setTimeout(() => {
+        pitelSDK.call(104);
+      }, 1000);
+    },
+    onUnregistered() {
+      console.log('SIP Unregister success!');
+    },
+    onCallCreated(remoteNumber) {
+      console.log('onCallCreated:', remoteNumber);
+    },
+    onCallReceived(remoteNumber) {
+      console.log('onCallReceived:', remoteNumber);
+      pitelSDK.accept();
+    },
+    onCallAnswered() {
+      console.log('onCallAnswered');
+    },
+    onCallHangup() {
+      console.log('onCallHangup');
+    },
+    onCallHold() {
+      console.log('onCallHold');
+    },
+  };
+  let pitelSDK = new PitelSDK('xxx', 'xxx', '103', sdkDelegates, sdkOptions);
+}
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  initSdk();
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
+
